d9d836b29f9e700df223fc03b076b5728ce0c10c
SamarV-121 <samarvispute121@pm.me>
2023-07-20 14:52:21 +0100

    mt6893-common: Link older libutils for PQ HAL
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Change-Id: I7ea86d3d7c68ba183b4838cb1f35a9fc58cfbf67

--- extract-files.py
+++ extract-files.py
@@ -49,6 +49,9 @@ blob_fixups: blob_fixups_user_type = {
 
     ('vendor/lib/libteei_daemon_vfs.so', 'vendor/lib64/libteei_daemon_vfs.so'): blob_fixup()
         .add_needed('liblog.so'),
+
+    'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.13-impl.so': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
