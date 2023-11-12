5de45ade3bcf7318dd9a8d099396b7ff31687471
Woomymy <woomy@woomy.be>
2023-11-12 13:20:42 +0100

    mt6893-common: Add a shim to link sensor blobs against `android.hardware.sensors@1.0-convert`
    * `libndksensorbridge` was converted to AIDL and is no longer linked to
      this static library that contains symbols required by our sensor
      blobs. Add a shim linked to `android.hardware.sensors@1.0-convert` to
      fix our sensors
    
    Change-Id: I969f97bb15d0abcc71f87352722995db3826e122
    Signed-off-by: Woomymy <woomy@woomy.be>

--- extract-files.py
+++ extract-files.py
@@ -39,6 +39,9 @@ blob_fixups: blob_fixups_user_type = {
 
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+
+    ('vendor/bin/mnld', 'vendor/lib64/libaalservice.so'): blob_fixup()
+        .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
