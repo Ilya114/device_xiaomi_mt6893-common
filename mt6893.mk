bb8b8c3b554cded863cbaced34c6b7437d04179d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-11-17 21:02:55 +0000

    mt6893-common: Build default health HIDL
    Change-Id: I72bf9b21274d27952997b9ddcb3f1ae1a7ad8013

--- mt6893.mk
+++ mt6893.mk
@@ -38,6 +38,12 @@ PRODUCT_PACKAGES += \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.mt6893 \
