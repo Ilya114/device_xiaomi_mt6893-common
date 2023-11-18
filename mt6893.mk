104084f655e839a97340dd645c41496b454e0e39
Lokesh Gidra <lokeshgidra@google.com>
2023-11-18 21:30:42 +0000

    mt6893-common: Inherit the Dalvik heap configuration for a 6GB device
    Change-Id: Ib6b1d7ad933fe3867dc6253e229a3c6470135e75

--- mt6893.mk
+++ mt6893.mk
@@ -6,6 +6,9 @@
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Dalvik VM Configuration
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
