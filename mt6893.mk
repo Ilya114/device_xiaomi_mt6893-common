2489363026c5f596936542a14a83c3b1d8be0bca
bengris32 <bengris32@protonmail.ch>
2023-12-20 00:36:24 +0000

    mt6893-common: Ship AOSP cgroups and task_profiles configs
    Change-Id: Ie74f85fa6328e6fa91d261c4aec257e5bb53ba0e
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6893.mk
+++ mt6893.mk
@@ -82,6 +82,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.example
 
+# Cgroup
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
