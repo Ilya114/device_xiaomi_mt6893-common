13407ee0a6c82c41d187882d45bbfd51e0f3b287
bengris32 <bengris32@protonmail.ch>
2024-02-21 19:57:17 +0000

    mt6893-common: Switch to MediaTek health AIDL implementation
    Change-Id: I81511392c9ecc43f879bb4716bade53611425883
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6893.mk
+++ mt6893.mk
@@ -105,8 +105,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.example \
-    android.hardware.health-service.example_recovery
+    android.hardware.health-service.mediatek \
+    android.hardware.health-service.mediatek-recovery
 
 # IFAA manager
 PRODUCT_PACKAGES += \
