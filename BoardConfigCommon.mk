83cebc88d7480ada0e46f0fa9f117bef17dbdf92
rio004 <riyon336@gmail.com>
2024-05-04 21:03:16 +0100

    mt6893-common: Inherit common Mediatek and Xiaomi framework compatibility matrix
    Change-Id: I8fbdc3e672064202b49844cba2b915ca9bf1e597
    Signed-off-by: rio004 <riyon336@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -33,6 +33,9 @@ TARGET_2ND_CPU_VARIANT := cortex-a55
 TARGET_NO_BOOTLOADER := true
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Kernel
