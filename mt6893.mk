275dec62c3c12da34762e76471e4698b4f21e5e0
rio004 <riyon336@gmail.com>
2025-07-11 00:28:20 +0100

    mt6893-common: Import Bluetooth stack
    Change-Id: I77534b013257c37bfbe625ce8217ae32c63035dd
    Signed-off-by: rio004 <riyon336@gmail.com>

--- mt6893.mk
+++ mt6893.mk
@@ -62,6 +62,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio-impl
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
