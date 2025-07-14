015898e82c1d91f075f928ceafed7a5ba19f2a62
bengris32 <bengris32@protonmail.ch>
2025-07-14 11:17:46 +0100

    mt6893-common: Switch to AIDL pixel thermal service
    * Config based off "dumpsys thermalservice" using stock thermal service.
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: Iff48b8dc3e968515758b88f348bdf2594d2dcf73

--- mt6893.mk
+++ mt6893.mk
@@ -195,6 +195,13 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek/libmtkperf_client \
     hardware/xiaomi
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
