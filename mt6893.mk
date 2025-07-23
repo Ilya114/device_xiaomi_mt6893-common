23b7b968776b1c244393ba896dfd1f56464bb69e
rio004 <riyon336@gmail.com>
2025-07-23 19:04:10 +0100

    mt6893-common: Initial common FrameworksRes overlay
    Change-Id: Ie4fd689d02e0d631b00e8468bed3b53ad2d94b8b
    Signed-off-by: rio004 <riyon336@gmail.com>

--- mt6893.mk
+++ mt6893.mk
@@ -116,6 +116,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResOverlayMT6893 \
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
