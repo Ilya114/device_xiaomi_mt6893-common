Transsion-MT6893-Resources/android_device_transsion_mt6893-common

mt6893-common: Patch libsource to load the old graphicsbuffer symbol
Change-Id: Ic8f1880379bf7adf07193842a710a104e434d470

extract-files.py
@@ -16,6 +16,8 @@ from extract_utils.main import (
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libsink.so': blob_fixup()
         .add_needed('libaudioclient_shim.so'),
+    'system_ext/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
     'vendor/bin/factory': blob_fixup()
         .replace_needed('android.hardware.light-V1-ndk_platform.so', 'android.hardware.light-V1-ndk.so'),
     ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()

Ilya114/device_xiaomi_mt6893-common

extract-files.py
@@ -58,6 +58,9 @@ blob_fixups: blob_fixups_user_type = {
 
     'system/lib64/libsink.so': blob_fixup()
         .add_needed('libaudioclient_shim.so'),
+
+    'system/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(