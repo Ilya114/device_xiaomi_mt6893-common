7cc7cddc137132ee3d5e12810ffefcec7b7849eb
ZiadTamer <ziadtamer150@gmail.com>
2026-03-11 17:39:46 +0200

    mt6893-common: Import keylayout & idc configs
    Change-Id: I61af7579aba52e2f03303db366c5c267ca80cee9
    Signed-off-by: ZiadTamer <ziadtamer150@gmail.com>

--- mt6893.mk
+++ mt6893.mk
@@ -139,6 +139,15 @@ PRODUCT_PACKAGES += \
 # IMS
 $(call inherit-product, vendor/mediatek/ims/ims.mk)
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/configs/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-goodix.idc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
